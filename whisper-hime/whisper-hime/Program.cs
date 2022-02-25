e7df370bd4e7c68677838d1892edf718e890d422
ShellyMon <1762889143@qq.com>
2022-02-25 15:24:16 +0800

    1.0.1
    创建连接

--- whisper-hime/whisper-hime/Program.cs
+++ whisper-hime/whisper-hime/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Sisters.WudiLib.WebSocket;
+
+var cqWebSocketEvent = new CqHttpWebSocketEvent("ws://127.0.0.1:6700/event"); // 创建 WebSocket 事件监听客户端。
+var httpApiClient = new CqHttpWebSocketApiClient("ws://127.0.0.1:6700/api"); // 创建 HTTP 通信客户端。
+cqWebSocketEvent.ApiClient = httpApiClient;
