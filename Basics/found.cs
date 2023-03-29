79c2b47dde1212363e753e6b8729fcfd4bd66592
ShellyMon <1762889143@qq.com>
2023-03-29 23:47:00 +0800

    删除没用的代码

--- Basics/found.cs
+++ Basics/found.cs
@@ -3,45 +3,24 @@ using Newtonsoft.Json;
 using SoraBot.Model;
 using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace SoraBot.Basics
 {
     public class ImageDownloadService
     {
-        public ImageDownloadService() { }
-
-        public static MatchCollection setuRegexMatches(string str,string Regex) {
-            Regex regex = new Regex(Regex);
-            return regex.Matches(str);
-        }
-        /// <summary>
-        /// 发送http Get请求
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        public static HttpWebResponse CreateGetHttpResponse(string url)
-        {
-            Thread.Sleep(20);
-            var request = WebRequest.Create(url) as HttpWebRequest;
-            request.Method = "GET";
-            request.ContentType = "application/x-www-form-urlencoded";//链接类型
-            //"Accept-Language": "zh-CN,zh;"
-            request.Accept = "zh-CN,zh;";
-            return request.GetResponse() as HttpWebResponse;
-        }
-        public static async Task<string> HttpGetAsync(string url)
+        internal static async Task<string> HttpGetAsync(string url)
         {
             var client = new HttpClient();
             return await client.GetStringAsync(url);
         }
+
         internal static async Task<LoliconApiResult<List<LoliconImageEntity>>> GetLoliconImage(string url)
         {
             var json = await HttpGetAsync(url);
             return JsonConvert.DeserializeObject<LoliconApiResult<List<LoliconImageEntity>>>(json);
         }
 
-        public static string GetResponseString(HttpWebResponse webresponse)
+        internal static string GetResponseString(HttpWebResponse webresponse)
         {
             using (Stream s = webresponse.GetResponseStream())
             {
@@ -50,7 +29,7 @@ namespace SoraBot.Basics
             }
         }
 
-        public static async Task<string> DownloadFileByAria(string url, IDictionary<string, object> options)
+        internal static async Task<string> DownloadFileByAria(string url, IDictionary<string, object> options)
         {
             var aria = new Aria2NetClient("http://127.0.0.1:6800/jsonrpc");
 
