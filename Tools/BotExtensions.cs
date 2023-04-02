b892e339cf28ddb31e074383bdc6658ec8ab90e4
ShellyMon <1762889143@qq.com>
2023-04-02 17:48:03 +0800

    添加扩展回复函数

---
+++ Tools/BotExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Sora.Entities;
+using Sora.EventArgs.SoraEvent;
+using System;
+using System.Threading.Tasks;
+
+namespace SoraBot.Tools
+{
+    internal static class BotExtensions
+    {
+        /// <summary>
+        /// 快速发送回复，只支持<c>GroupMessageEventArgs</c>和<c>PrivateMessageEventArgs</c>这两种类型的事件。
+        /// </summary>
+        /// <param name="e">参数</param>
+        /// <param name="message">消息</param>
+        /// <param name="timeout">超时</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static ValueTask<(ApiStatus apiStatus, int messageId)> Reply(this BaseSoraEventArgs e, MessageBody message, TimeSpan? timeout = null)
+        {
+            if (e is PrivateMessageEventArgs p)
+                return p.Reply(message, timeout);
+
+            if (e is GroupMessageEventArgs g)
+                return g.Reply(message, timeout);
+
+            throw new NotImplementedException();
+        }
+    }
+}
