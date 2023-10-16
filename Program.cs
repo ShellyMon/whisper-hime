c79bddf65efa18c0b57964d5063d170164e27774
ShellyMon <1762889143@qq.com>
2023-10-16 11:47:42 +0800

    删除不相干代码

--- Program.cs
+++ Program.cs
@@ -159,65 +159,6 @@ namespace Sora
                 logger.LogError(e, "BotException");
             }
 
-            var botClient = new TelegramBotClient("5714174864:AAED6t1dMJ8x6vdZgNH5tM03M0BDlVPLg-0");
-
-            //var me = await botClient.GetMeAsync();
-            using CancellationTokenSource cts = new();
-
-            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
-            ReceiverOptions receiverOptions = new()
-            {
-                AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
-            };
-
-            botClient.StartReceiving(
-                updateHandler: HandleUpdateAsync,
-                pollingErrorHandler: HandlePollingErrorAsync,
-                receiverOptions: receiverOptions,
-                cancellationToken: cts.Token
-            );
-
-            var me = await botClient.GetMeAsync();
-
-            Console.WriteLine($"Start listening for @{me.Username}");
-            Console.ReadLine();
-
-            // Send cancellation request to stop bot
-            cts.Cancel();
-
-            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-            {
-                // Only process Message updates: https://core.telegram.org/bots/api#message
-                if (update.Message is not { } message)
-                    return;
-                // Only process text messages
-                if (message.Text is not { } messageText)
-                    return;
-
-                var chatId = message.Chat.Id;
-
-                Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
-
-                // Echo received message text
-                Message sentMessage = await botClient.SendTextMessageAsync(
-                    chatId: chatId,
-                    text: "You said:\n" + messageText,
-                    cancellationToken: cancellationToken);
-            }
-
-            Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-            {
-                var ErrorMessage = exception switch
-                {
-                    ApiRequestException apiRequestException
-                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                    _ => exception.ToString()
-                };
-
-                Console.WriteLine(ErrorMessage);
-                return Task.CompletedTask;
-            }
-
             // TODO: 堵塞
             await Task.Delay(-1);
         }
