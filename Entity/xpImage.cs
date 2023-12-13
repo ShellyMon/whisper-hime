8ba464fe25484ad4ec1d0c9140015d9acc8a3282
ShellyMon <1762889143@qq.com>
2023-12-13 14:30:34 +0800

    个性化模型

---
+++ Entity/xpImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhisperHime.Entity
+{
+    public class xpImage
+    {
+        /// <summary>
+        /// qq号
+        /// </summary>
+        public long qq { get; set; }
+
+        /// <summary>
+        /// qq昵称
+        /// </summary>
+        public string qqName { get; set; }
+
+        /// <summary>
+        /// 获取名称
+        /// </summary>
+        public string xpName { get; set; }
+    }
+}
